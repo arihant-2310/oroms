f229e705a6c6d814f019ea549b9a941da5e21346
arihant-2310 <arihant2310@gmail.com>
2020-04-13 00:26:36 +0530

    service comments removed

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -16,11 +16,7 @@ export class RoomsService {
     async getRooms(filterDto:GetRoomsFilterDto):Promise<Room[]>{
         return this.roomRepository.getRooms(filterDto);
     }
-    // private rooms:Room[] = [];
-
-    // getAllRooms(): Room[]{
-    //     return this.rooms;
-    // }
+    
     async getRoomById(id:number):Promise<Room>{
         const found = await this.roomRepository.findOne(id);
         if(!found) 
@@ -29,31 +25,7 @@ export class RoomsService {
         } 
         return found;
     }
-    // getRoomById(id:string):Room{
-    //     const found= this.rooms.find(room=> room.id===id) ;
-    //     if(!found) 
-    //     {
-    //         throw new NotFoundException(`Room with id ${id} not found.`);
-    //     }      
-    //     return found;
-    // }
-
-    // getRoomsWithFilters(filterDto:GetRoomsFilterDto):Room[]{
-    //     const {category, search} = filterDto;
-    //     let rooms = this.getAllRooms();
-    //     if(category)
-    //     {
-    //         rooms = rooms.filter(room=>room.category===category);
-    //     }
-    //     if(search){ 
-    //         rooms = rooms.filter(room =>
-    //             room.title.includes(search) ||
-    //             room.address.includes(search)||
-    //             room.address.includes(search),    
-    //         );
-    //     }
-    //     return rooms;
-    // }
+    
 
     async createRoom(createRoomDto: CreateRoomDto){
         return this.roomRepository.createRoom(createRoomDto);
@@ -72,34 +44,5 @@ export class RoomsService {
         await room.save();
         return room;
     }
-    // createRoom(
-    //     createRoomDto: CreateRoomDto
-    // ):Room
-    // {
-    //     const {title,address,features,description,category,photos,cost,policy}=createRoomDto;
-    //     const room:Room = {
-    //         id: uuid(),
-    //         title,
-    //         address,
-    //         features,
-    //         description,
-    //         category,
-    //         photos,
-    //         cost,
-    //         policy,
-    //         status: RoomStatus.AVAILABLE,
-    //     };
-    //     this.rooms.push(room);
-    //     return room;
-    // }
-
-    // deleteRoom(id:string):void{
-    //     const found = this.getRoomById(id);
-    //     this.rooms =this.rooms.filter(room=> room.id !== found.id);
-    // }
-    // updateRoomStatus(id:string,status:RoomStatus):Room{
-    //     const room = this.getRoomById(id);
-    //     room.status= status;
-    //     return room;
-    // }
+    
 }
